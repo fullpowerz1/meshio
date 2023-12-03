c7d605eda7323ebc6716beb138006013dfc9549a
Taiki Sonoyama <109128636+fullpowerz1@users.noreply.github.com>
2023-12-03 00:01:39 +0000

    sin

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "homes#top"
   get 'homes/about' => 'homes#about',as: 'about'
-  resources :post_images, only: [:new, :create, :index, :show, :destroy]
+  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+    resource :post_comments, only: [:create]
+  end
+
   resources :users, only: [:show, :edit, :update]
 end
